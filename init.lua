8cc734c7fd9e485ffd76841ec0e89aafc0b43bf9
y <y@y>
2019-04-25 00:16:04 +0100

    cleanup

--- init.lua
+++ init.lua
@@ -1,23 +1,29 @@
-minetest.log('action', '[cs_waypoints] CSM loading...')
+local mod_name = minetest.get_current_modname()
+
+local function log(level, message)
+    minetest.log(level, ('[%s] %s'):format(mod_name, message))
+end
+
+log('action', 'CSM loading...')
 
 local mod_storage = minetest.get_mod_storage()
 
 local waypoints
-if string.find(mod_storage:get_string("waypoints"), "return") then
-    waypoints = minetest.deserialize(mod_storage:get_string("waypoints"))
+if string.find(mod_storage:get_string('waypoints'), 'return') then
+    waypoints = minetest.deserialize(mod_storage:get_string('waypoints'))
 else
     waypoints = {}
 end
 
 
-function safe(func)
-    -- run a function without crashing the game.
+local function safe(func)
+    -- wrap a function w/ logic to avoid crashing the game
     local f = function(...)
         local status, out = pcall(func, ...)
         if status then
             return out
         else
-            minetest.debug("Error (func):  " .. out)
+            log('warning', 'Error (func):  ' .. out)
             return nil
         end
     end
@@ -37,7 +43,9 @@ end
 
 local function pairsByKeys(t, f)
     local a = {}
-    for n in pairs(t) do table.insert(a, n) end
+    for n in pairs(t) do
+        table.insert(a, n)
+    end
     table.sort(a, f)
     local i = 0
     return function()
@@ -56,48 +64,50 @@ local function lc_cmp(a, b)
 end
 
 
-minetest.register_chatcommand('wps', {
+local function tostring_point(point)
+    return ('%i %i %i'):format(round(point.x), round(point.y), round(point.z))
+end
+
+
+minetest.register_chatcommand('wp_s', {
     params = '<name>',
     description = 'set a waypoint',
     func = safe(function(param)
+        waypoints = minetest.deserialize(mod_storage:get_string('waypoints'))
         local point = minetest.localplayer:get_pos()
         waypoints[param] = point
-        mod_storage:set_string("waypoints", minetest.serialize(waypoints))
+        mod_storage:set_string('waypoints', minetest.serialize(waypoints))
 
-        local x = tostring(round(point['x']))
-        local y = tostring(round(point['y']))
-        local z = tostring(round(point['z']))
-        local pos = x .. ' ' .. y .. ' ' .. z
-        local msg = 'set waypoint "' .. param .. '" to "' .. pos .. '"'
-
-        minetest.display_chat_message(msg)
+        minetest.display_chat_message(
+            ('set waypoint "%s" to "%s"'):format(param, tostring_point(point))
+        )
     end),
 })
 
 
-minetest.register_chatcommand('wprm', {
+minetest.register_chatcommand('wp_rm', {
     params = '<name>',
     description = 'remove a waypoint',
     func = safe(function(param)
+        waypoints = minetest.deserialize(mod_storage:get_string('waypoints'))
         waypoints[param] = nil
-        mod_storage:set_string("waypoints", minetest.serialize(waypoints))
+        mod_storage:set_string('waypoints', minetest.serialize(waypoints))
 
-        minetest.display_chat_message('removed waypoint "' .. param .. '"')
+        minetest.display_chat_message(
+            ('removed waypoint "%s"'):format(param)
+        )
     end),
 })
 
 
-minetest.register_chatcommand('wpls', {
+minetest.register_chatcommand('wp_ls', {
     params = '',
     description = 'lists waypoints',
     func = safe(function(_)
         for name, point in pairsByKeys(waypoints, lc_cmp) do
-            local x = tostring(round(point['x']))
-            local y = tostring(round(point['y']))
-            local z = tostring(round(point['z']))
-            local pos = x .. ' ' .. y .. ' ' .. z
-
-            minetest.display_chat_message(name .. ' ' .. pos)
+            minetest.display_chat_message(
+                ('%s -> %s'):format(name, tostring_point(point))
+            )
         end
     end),
 })
@@ -107,34 +117,22 @@ minetest.register_chatcommand('tw', {
     params = '(<playernamename>) <waypoint>',
     description = 'teleport (a player) to a waypoint',
     func = safe(function(param)
-        if string.match(param, "^(%S+)%s+(%S+)$") ~= nil then
-            local playername, wpname = string.match(param, "^(%S+)%s+(%S+)$")
+        local playername, wpname = string.match(param, '^(%S+)%s+(%S+)$')
+        if playername and wpname then
             local waypoint = waypoints[wpname]
-
             if waypoint ~= nil then
-                local x = tostring(round(waypoint['x']))
-                local y = tostring(round(waypoint['y']))
-                local z = tostring(round(waypoint['z']))
-                local pos = x .. ' ' .. y .. ' ' .. z
-
-                minetest.run_server_chatcommand("teleport", playername .. ' ' .. pos)
+                local args = ('%s %s'):format(playername, tostring_point(point))
+                minetest.run_server_chatcommand('teleport', args)
             else
-                minetest.display_chat_message('waypoint "' .. wpname .. '" not found.')
+                minetest.display_chat_message(('waypoint "%s" not found.'):format(wpname))
             end
-
         else
             local wpname = param
             local waypoint = waypoints[wpname]
             if waypoint ~= nil then
-                local x = tostring(round(waypoint['x']))
-                local y = tostring(round(waypoint['y']))
-                local z = tostring(round(waypoint['z']))
-                local pos = x .. ' ' .. y .. ' ' .. z
-
-                minetest.run_server_chatcommand("teleport", pos)
-                -- minetest.localplayer:set_pos(waypoint)
+                minetest.run_server_chatcommand('teleport', tostring_point(point))
             else
-                minetest.display_chat_message('waypoint "' .. wpname .. '" not found.')
+                minetest.display_chat_message(('waypoint "%s" not found.'):format(wpname))
             end
         end
 
@@ -146,26 +144,23 @@ minetest.register_chatcommand('tr', {
     params = '<ELEV> | <PLAYER> | <PLAYER> <ELEV>',
     description = '/teleport (a player) to a random location',
     func = safe(function(param)
-        minetest.log('info', '[cs_waypoints] rt "' .. param .. '"')
-
         local x = math.random(-30912, 30927)
         local y = math.random(-30912, 30927)
         local z = math.random(-30912, 30927)
         local name = ''
 
-        if string.match(param, "^([%a%d_-]+) (%d+)$") ~= nil then
+        if string.match(param, '^([%a%d_-]+) (%d+)$') ~= nil then
             name, y = string.match(param, "^([%a%d_-]+) (%d+)$")
 
-        elseif string.match(param, "^([%d-]+)$") then
-            y = string.match(param, "^([%d-]+)$")
+        elseif string.match(param, '^([%d-]+)$') then
+            y = string.match(param, '^([%d-]+)$')
 
-        elseif string.match(param, "^([%a%d_-]+)$") ~= nil then
-            name = string.match(param, "^([%a%d_-]+)$")
+        elseif string.match(param, '^([%a%d_-]+)$') ~= nil then
+            name = string.match(param, '^([%a%d_-]+)$')
         end
 
-        local pos = tostring(x) .. ' ' .. tostring(y) .. ' ' .. tostring(z)
-
-        minetest.run_server_chatcommand("teleport", name .. ' ' .. pos)
+        local args = ('%s %s %s %s'):format(name, x, y, z)
+        minetest.run_server_chatcommand('teleport', args)
     end),
 })
 
